e4296e2cd7a522cc2b76ec72c8a00d985a26e378
MegaPiggy <34462599+MegaPiggy@users.noreply.github.com>
2021-06-01 10:51:44 -0400

    Started finding a way to create levels.

---
+++ Registries/LevelRegistry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SALT.Registries
+{
+    public static class LevelRegistry
+    {
+        public static Scene CreateLevel(string name, CreateSceneParameters parameters) => SceneManager.CreateScene(name, parameters);
+    }
+}
