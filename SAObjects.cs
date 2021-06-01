7f7ee13de3a16fca07dc300407db2994959e4e06
MegaPiggy <34462599+MegaPiggy@users.noreply.github.com>
2021-05-31 23:17:41 -0400

    Added base objects

--- SAObjects.cs
+++ SAObjects.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using SALT.Extensions;
 using SALT.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,16 +13,90 @@ namespace SALT
 	public static class SAObjects
 	{
 		public static GameObject OptionPrefab;
+		public static GameObject Block;
+		public static GameObject Book;
+		public static GameObject Button;
+		public static GameObject Carrot;
+		public static GameObject CloudSolid;
+		public static GameObject CloudSpawner;
+		public static GameObject Desk;
+		public static GameObject Folder;
+		public static GameObject LevelButton;
+		public static GameObject Moon;
+		public static GameObject NukeButton;
+		public static GameObject GenericButton;
+		public static GameObject Spawnpoint; 
 
 
 		// Static Constructors to load the base objects
 		static SAObjects()
 		{
+			Block = GetInstInactive("Block");
+			Block.name = "Block";
+			Book = GetInstInactive("Book");
+			Book.name = "Book";
+			Button = GetInstInactive("Button", go => go.transform.parent.name != "Button");
+			Button.name = "Button";
+			Carrot = GetInstInactive("CarrotPlatform");
+			Carrot.name = "CarrotPlatform";
+			CloudSolid = GetInstInactive("CloudPlatform_Solid");
+			CloudSolid.name = "CloudPlatform_Solid";
+			CloudSpawner = GetInstInactive("CloudPlatformSpawner");
+			CloudSpawner.name = "CloudPlatformSpawner";
+			Desk = GetInstInactive("Desk (1)");
+			Desk.name = "Desk";
+			Folder = GetInstInactive("folder");
+			Folder.name = "folder";
+			LevelButton = GetInstInactive("LevelButton");
+			LevelButton.name = "LevelButton";
+			Moon = GetInstInactive("Moon");
+			Moon.name = "Moon";
+			NukeButton = GetInstInactive("NukeButton");
+			NukeButton.name = "NukeButton";
+			GenericButton = GetInstInactive("QuitButton");
+			GenericButton.name = "GenericButton";
+			GenericButton.GetComponent<GenericButtonScript>().RemoveAllMethods();
+			Spawnpoint = GetInstInactive("Spawnpoint");
+			Spawnpoint.name = "Spawnpoint";
+			//DumpUtils.DumpObject(Block, "SAObjects2");
+			//DumpUtils.DumpObject(Button, "SAObjects2");
+			//DumpUtils.DumpObject(Carrot, "SAObjects2");
+			//DumpUtils.DumpObject(CloudSolid, "SAObjects2");
+			//DumpUtils.DumpObject(CloudSpawner, "SAObjects2");
+			//DumpUtils.DumpObject(Desk, "SAObjects2");
+			//DumpUtils.DumpObject(Folder, "SAObjects2");
+			//DumpUtils.DumpObject(LevelButton, "SAObjects2");
+			//DumpUtils.DumpObject(Moon, "SAObjects2");
+			//DumpUtils.DumpObject(NukeButton, "SAObjects2");
+			//DumpUtils.DumpObject(GenericButton, "SAObjects2");
+			//DumpUtils.DumpObject(Spawnpoint, "SAObjects2");
+			//foreach (GameObject gameObject in GetAll<GameObject>())
+			//{
+			//	DumpUtils.DumpObject(gameObject, "SAObjects");
+			//}
 		}
 
 		public static GameObject[] GetAllRootGameObjects() => SceneManager.GetActiveScene().GetRootGameObjects();
 
 		public static GameObject GetRootGameObject(string name) => GetAllRootGameObjects().FirstOrDefault(go => go.name == name);
+
+		/// <summary>
+		/// Gets an object of a type by its name and a custom condition.
+		/// </summary>
+		/// <typeparam name="T">Type to search</typeparam>
+		/// <param name="name">Name to search for</param>
+		/// <param name="predicate">Other condition</param>
+		/// <returns>Object found or null if nothing is found</returns>
+		public static T Get<T>(string name, System.Predicate<T> predicate) where T : Object
+		{
+			foreach (T found in Resources.FindObjectsOfTypeAll<T>())
+			{
+				if (found.name.Equals(name) && predicate(found))
+					return found;
+			}
+
+			return null;
+		}
 
 		/// <summary>
 		/// Gets an object of a type by its name
@@ -64,6 +139,57 @@ namespace SALT
 		public static List<T> GetAll<T>() where T : Object
 		{
 			return new List<T>(Resources.FindObjectsOfTypeAll<T>());
+		}
+
+		/// <summary>
+		/// Gets an instance of a gameobject by its name and a custom condition.
+		/// </summary>
+		/// <param name="name">Name to search for</param>
+		/// <param name="predicate">Other condition</param>
+		/// <returns>Instance of gameobject found or null if nothing is found</returns>
+		public static GameObject GetInstInactive(string name, System.Predicate<GameObject> predicate)
+		{
+			foreach (GameObject found in Resources.FindObjectsOfTypeAll<GameObject>())
+			{
+				if (found.name.Equals(name) && predicate(found))
+					return GameObjectUtils.InstantiateInactive(found);
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Gets an instance of a gameobject by its name
+		/// </summary>
+		/// <param name="name">Name to search for</param>
+		/// <returns>Instance of gameobject found or null if nothing is found</returns>
+		public static GameObject GetInstInactive(string name)
+		{
+			foreach (GameObject found in Resources.FindObjectsOfTypeAll<GameObject>())
+			{
+				if (found.name.Equals(name))
+					return GameObjectUtils.InstantiateInactive(found);
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Gets an instance of an object of a type by its name and a custom condition.
+		/// </summary>
+		/// <typeparam name="T">Type to search</typeparam>
+		/// <param name="name">Name to search for</param>
+		/// <param name="predicate">Other condition</param>
+		/// <returns>Instance of object found or null if nothing is found</returns>
+		public static T GetInst<T>(string name, System.Predicate<T> predicate) where T : Object
+		{
+			foreach (T found in Resources.FindObjectsOfTypeAll<T>())
+			{
+				if (found.name.Equals(name) && predicate(found))
+					return Object.Instantiate(found);
+			}
+
+			return null;
 		}
 
 		/// <summary>
