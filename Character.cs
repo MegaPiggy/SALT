b408c6c180f5c385ff35f8e2f104eb1e8588b1b9
MegaPiggy <34462599+MegaPiggy@users.noreply.github.com>
2021-09-11 22:16:35 -0400

    Ollie & Reine

--- Character.cs
+++ Character.cs
@@ -9,51 +9,59 @@ namespace SALT
     /// <summary>
     /// An enum value for the characters in game.
     /// </summary>
-    public enum Character
+    public enum Character : int
     {
         /// <summary>
         /// A detective vtuber.
         /// </summary>
-        AMELIA,
+        AMELIA = 0,
         /// <summary>
         /// A shark vtuber.
         /// </summary>
-        GURA,
+        GURA = 1,
         /// <summary>
         /// A dog vtuber.
         /// </summary>
-        KORONE,
+        KORONE = 2,
         /// <summary>
         /// A cat vtuber.
         /// </summary>
-        OKAYU,
+        OKAYU = 3,
         /// <summary>
         /// Amelia but with a moustache and angry face.
         /// </summary>
-        AMELIA_MOUSTACHE,
+        AMELIA_MOUSTACHE = 4,
         /// <summary>
         /// Buff version of gura.
         /// </summary>
-        GURA_BUFF,
+        GURA_BUFF = 5,
         /// <summary>
         /// A cow that is supposed to the be the game creator Kevin.
         /// </summary>
-        KEVIN,
+        KEVIN = 6,
         /// <summary>
         /// Subaru as a duck. Popularized in a meme during May 2021.
         /// </summary>
-        SHUBA,
+        SHUBA = 7,
         /// <summary>
         /// Cat version of gura.
         /// </summary>
-        GURA_CAT,
+        GURA_CAT = 8,
         /// <summary>
         /// A dragon vtuber.
         /// </summary>
-        COCO,
+        COCO = 9,
+        /// <summary>
+        /// A zombie vtuber.
+        /// </summary>
+        OLLIE = 10,
+        /// <summary>
+        /// A peacock vtuber.
+        /// </summary>
+        REINE = 11,
         /// <summary>
         /// A character with nothing attached.
         /// </summary>
-        NONE// = -1,
+        NONE = 12// = -1,
     }
 }
