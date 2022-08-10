57d0ed76f6d11c8e9b95792720c010f153b8d289
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-10 11:15:08 -0400

    Add pair

---
+++ Pair.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public struct Pair<A, B>
+    {
+        private A first;
+        private B second;
+        public A First => first;
+        public B Second => second;
+
+        public Pair(A first, B second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append('[');
+            if (First != null)
+                stringBuilder.Append(First.ToString());
+
+            stringBuilder.Append(", ");
+            if (Second != null)
+                stringBuilder.Append(Second.ToString());
+
+            stringBuilder.Append(']');
+            return stringBuilder.ToString();
+        }
+
+        public static implicit operator KeyValuePair<A, B>(Pair<A, B> pair) => new KeyValuePair<A, B>(pair.First, pair.Second);
+        public static implicit operator Pair<A, B>(KeyValuePair<A, B> pair) => new Pair<A, B>(pair.Key, pair.Value);
+    }
+}
